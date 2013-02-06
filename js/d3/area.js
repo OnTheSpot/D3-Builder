c48375cf9bcb6343531e724b50cd0c9a85f0d574
Daniel Thomson <dansdom@gmail.com>
2013-02-06 16:29:19 +1100

    minor updates to data handling

--- js/d3/area.js
+++ js/d3/area.js
@@ -415,7 +415,7 @@ var Extend = Extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
             else {
                 // the data is passed straight into the plugin form either a function or a data object
                 // I expect a JSON object here
-                container.data = container.opts.data;
+                container.data = container.parseData(container.opts.data);
                 container.updateChart(); 
             }    
         },
